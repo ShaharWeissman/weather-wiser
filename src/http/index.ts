22a9b74674ca07b56bc605c26b4b97c9c8a64e82
shahar-weissman <shaharweissman89@gmail.com>
2023-12-25 17:06:00 +0200

    Update API endpoint and import API_KEY from config

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,10 +1,27 @@
-const apiUrl =
-  "http://dataservice.accuweather.com/locations/v1/cities/autocomplete";
-const apiKey = "i3fqvI3PsZLdmfu3VIIY9UOsS1mxnACx";
+import { API_KEY } from "../components/config/consts";
+console.log("🚀 ~ file: index.ts:2 ~ API_KEY:", API_KEY);
+
+const apiUrl = "http://dataservice.accuweather.com";
 
 export async function cityLookup(cityStr: string) {
   const response = await fetch(
-    `${apiUrl}?q=${cityStr}&apiKey=${apiKey}`
+    `${apiUrl}/locations/v1/cities/autocomplete?q=${cityStr}&apiKey=${API_KEY}`
+  );
+  const data = await response.json();
+  return data;
+}
+
+export async function getCurrentWeather(locationKey: string) {
+  const response = await fetch(
+    `${apiUrl}/currentconditions/v1/${locationKey}?apikey=${API_KEY}`
+  );
+  const data = await response.json();
+  return data;
+}
+
+export async function getDailyForecast(locationKey: string, isMetric: boolean) {
+  const response = await fetch(
+    `${apiUrl}/forecasts/v1/daily/5day/${locationKey}?apikey=${API_KEY}&metric=${isMetric}`
   );
   const data = await response.json();
   return data;
