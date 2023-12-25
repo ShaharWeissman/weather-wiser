2dc8963df2b793e457f0b601b92fd6311bb34f34
shahar-weissman <shaharweissman89@gmail.com>
2023-12-25 17:03:14 +0200

    Add dotenv package and environment variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_API_KEY: string;
+  readonly VITE_CITY_URL: string;
+  readonly VITE_CURRENT_WEATHER_URL: string;
+  readonly VITE_FIVE_DAYS_FORECAST_URL: string;
+}
