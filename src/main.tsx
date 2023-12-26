a54e5cc9f94eb81516ff1a7709521e0ed5658a1d
shahar-weissman <shaharweissman89@gmail.com>
2023-12-26 14:04:15 +0200

    Remove commented code for WeatherApp component

--- src/main.tsx
+++ src/main.tsx
@@ -12,13 +12,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </BrowserRouter>
   </Provider>
 );
-// import ReactDOM from "react-dom/client";
-// import { Provider } from "react-redux";
-// import { store } from "./store/store";
-// import WeatherApp from "./WeatherApp"; // Assuming Main is the file where you define the above Main component
-
-// ReactDOM.createRoot(document.getElementById("root")!).render(
-//   <Provider store={store}>
-//     <WeatherApp />
-//   </Provider>
-// );
