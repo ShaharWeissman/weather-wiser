d3d39201f7981919fadcc804c4e86f645aef07c4
shahar-weissman <shaharweissman89@gmail.com>
2023-12-26 14:28:47 +0200

    Add sourceMap and base URL for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/weather-wiser/",
   server: {
     hmr: {
       overlay: false, // for the overlay to work properly with React
     },
   },
-})
+});
