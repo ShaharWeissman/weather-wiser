2fc15ada7cbc4bc6d81973bd3f393ce59adc3ffd
shahar-weissman <shaharweissman89@gmail.com>
2024-01-07 18:04:38 +0200

    Add forecast icon to ForecastCard component

--- src/components/weather-details/location/5-days-forecast/ForecastCard.tsx
+++ src/components/weather-details/location/5-days-forecast/ForecastCard.tsx
@@ -4,15 +4,21 @@ import "./ForecastCard.css";
 type ForecastProps = {
   day: string;
   temperature: number;
+  forecastIcon: string;
 };
 
-function ForecastCard({ day, temperature }: ForecastProps): JSX.Element {
+function ForecastCard({
+  day,
+  temperature,
+  forecastIcon,
+}: ForecastProps): JSX.Element {
   const isMetric = useAppSelector((state) => state.cities.isMetric);
   return (
     <div className="forecast-card">
       <div className="background-image"></div>
       <div className="card-content">
         <h5 className="weather-day">{day}</h5>
+        <img src={forecastIcon} alt="weather-icon" />
         <h6 className="weather-temperature">
           {temperature} &nbsp;{isMetric ? "C" : "F"}°
         </h6>
