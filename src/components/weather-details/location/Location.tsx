b2ee0fbdd3c14fae5ad7d4412e92d638057978d1
shahar-weissman <shaharweissman89@gmail.com>
2024-01-07 18:05:18 +0200

    Refactor weather location component layout

--- src/components/weather-details/location/Location.tsx
+++ src/components/weather-details/location/Location.tsx
@@ -1,13 +1,22 @@
+import { Grid } from "@mui/material";
 import WeatherForecast from "./5-days-forecast/WeatherForecast";
 import CurrentWeather from "./current-weather/CurrentWeather";
 import "./Location.css";
 
 function Location(): JSX.Element {
   return (
-    <div className="weather-container">
-      <CurrentWeather />
-      <WeatherForecast />
-    </div>
+    <Grid
+      container
+      spacing={2}
+      direction="column"
+      className="weather-container">
+      <Grid xs={12} sm={6} md={8} lg={10}>
+        <CurrentWeather />
+      </Grid>
+      <Grid xs={12} sm={6} md={8} lg={10}>
+        <WeatherForecast />
+      </Grid>
+    </Grid>
   );
 }
 export default Location;
