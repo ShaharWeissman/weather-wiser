dbc08b6f6e76a6f09d52fc52d76adfb03e9a395a
shahar-weissman <shaharweissman89@gmail.com>
2024-01-09 18:27:07 +0200

    Remove RootState interface

--- src/store/rootState.ts
+++
@@ -1,8 +0,0 @@
-import { CitiesState } from "../types/ICity";
-import { DailyForecastDetailsState } from "../types/IDailyForecast";
-import { LocationDetailsState } from "../types/ICurrentWeather";
-export interface RootState {
-  cities: CitiesState;
-  locationDetails: LocationDetailsState;
-  dailyForecastDetails: DailyForecastDetailsState;
-}
