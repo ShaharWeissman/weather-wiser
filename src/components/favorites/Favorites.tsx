0757155e20cb07b6d98471644fccca9060cc834d
shahar-weissman <shaharweissman89@gmail.com>
2024-03-02 18:47:28 +0200

    Update Favorites component styling

--- src/components/favorites/Favorites.tsx
+++ src/components/favorites/Favorites.tsx
@@ -1,19 +1,18 @@
-import "./Favorites.css";
 import FavoriteCard from "./FavoriteCard";
 import { useAppSelector } from "../../store/store";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
 
 function Favorites(): JSX.Element {
   const favorites = useAppSelector((state) => state.cities.favorites);
-
+  const theme = useTheme();
   return (
     <Box
       sx={{
         display: "flex",
         flexDirection: "row",
         alignItems: "center",
-        backgroundColor: "var(--cont-col)",
-        bgcolor: "#f8f8f0",
+        bgcolor: theme.palette.primary.main,
+        color: theme.palette.secondary.main,
         padding: "40px",
         borderRadius: "12px",
         boxShadow: "0 2px 5px rgba(0, 0, 0, 0.2)",
